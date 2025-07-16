f05321833728ef65132fa9d52e88cabfaab0119f
ILYAS1288 <elibubalteen@gmail.com>
2025-07-16 18:37:08 +0500

    change admin

--- server/routes/adminUser.js
+++ server/routes/adminUser.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  // Dummy login logic
+  // Dummy login
   if (username === 'admin@example.com' && password === '123456') {
     return res.status(200).json({ message: 'Login successful', token: 'fake-jwt-token' });
   } else {
